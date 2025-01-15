43efa8803822825047b24186901eae5c07465c41
Robin Stumm <serverkorken@gmail.com>
2025-01-15 12:37:49 +0100

    prepare for garnix
    Garnix does not build the `hydraJobs` output
    so move them all into the `checks` output,
    adhering to the flake output schema
    because garnix does not build nested attrsets.

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -9,6 +9,17 @@ in
 [
   (
     # Docs for project.flake: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectoutflake
-    project.flake
+    project.flake // {
+      checks = let
+        # https://github.com/numtide/flake-utils/issues/121#issuecomment-2589899217
+        recurseIntoDeepAttrs = attrs:
+          lib.recurseIntoAttrs (lib.mapAttrs (_: v:
+            if builtins.typeOf v == "set" && !lib.isDerivation v
+            then recurseIntoDeepAttrs v
+            else v
+          ) attrs);
+      in
+        inputs.iogx.inputs.flake-utils.lib.flattenTree (recurseIntoDeepAttrs project.flake.hydraJobs);
+    }
   )
 ]
