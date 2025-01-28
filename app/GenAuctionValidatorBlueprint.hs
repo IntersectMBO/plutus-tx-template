056efcfa564a59136a89325ca55dcfa6c21a224e
Yura Lazaryev <Yuriy.Lazaryev@iohk.io>
2025-01-28 11:02:35 +0100

    Update the Auction validator blueprint to match updated Auction Validator.

--- app/GenAuctionValidatorBlueprint.hs
+++ app/GenAuctionValidatorBlueprint.hs
@@ -1,38 +1,54 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE DerivingStrategies    #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NumericUnderscores    #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module Main where
 
-import AuctionValidator
-import Data.ByteString.Short qualified as Short
-import Data.Set qualified as Set
-import PlutusLedgerApi.Common (serialiseCompiledCode)
-import PlutusTx.Blueprint
-import System.Environment (getArgs)
+import           AuctionValidator
+import qualified Data.ByteString.Short       as Short
+import qualified Data.Set                    as Set
+import           PlutusLedgerApi.Common      (serialiseCompiledCode)
+import qualified PlutusLedgerApi.V1.Crypto   as Crypto
+import qualified PlutusLedgerApi.V1.Time     as Time
+import qualified PlutusLedgerApi.V1.Value    as Value
+import           PlutusTx.Blueprint
+import           PlutusTx.Builtins.HasOpaque (stringToBuiltinByteStringHex)
+import           System.Environment          (getArgs)
 
 auctionParams :: AuctionParams
 auctionParams =
   AuctionParams
-    { apSeller = error "Replace with seller public key hash"
-    , apCurrencySymbol = error "Replace with currency symbol"
-    , apTokenName = error "Replace with token name"
-    , apMinBid = 100
-    , apEndTime = error "Replace with the auction's end time"
+    { apSeller =
+        -- Replace with the hex-encoded seller's public key hash:
+        Crypto.PubKeyHash (
+          stringToBuiltinByteStringHex
+            "0000000000000000000000000000000000000000\
+            \0000000000000000000000000000000000000000"
+          )
+    , apCurrencySymbol =
+        -- Replace with your desired currency symbol (minting policy hash):
+        Value.CurrencySymbol (
+          stringToBuiltinByteStringHex
+            "00000000000000000000000000000000000000000000000000000000"
+          )
+    , apTokenName =
+        -- Replace with your desired token name:
+        Value.tokenName "MY_TOKEN"
+    , apMinBid =
+        -- Minimal bid in lovelace:
+        100
+    , apEndTime =
+        -- Replace with your desired end time in milliseconds:
+        Time.fromMilliSeconds 1_725_227_091_000
     }
 
 myContractBlueprint :: ContractBlueprint
@@ -41,14 +57,16 @@ myContractBlueprint =
     { contractId = Just "auction-validator"
     , contractPreamble = myPreamble
     , contractValidators = Set.singleton myValidator
-    , contractDefinitions = deriveDefinitions @[AuctionParams, AuctionDatum, AuctionRedeemer]
+    , contractDefinitions =
+        deriveDefinitions @[AuctionParams, AuctionDatum, AuctionRedeemer]
     }
 
 myPreamble :: Preamble
 myPreamble =
   MkPreamble
     { preambleTitle = "Auction Validator"
-    , preambleDescription = Just "Blueprint for a Plutus script validating auction transactions"
+    , preambleDescription =
+        Just "Blueprint for a Plutus script validating auction transactions"
     , preambleVersion = "1.0.0"
     , preamblePlutusVersion = PlutusV2
     , preambleLicense = Just "MIT"
@@ -58,7 +76,8 @@ myValidator :: ValidatorBlueprint referencedTypes
 myValidator =
   MkValidatorBlueprint
     { validatorTitle = "Auction Validator"
-    , validatorDescription = Just "Plutus script validating auction transactions"
+    , validatorDescription =
+        Just "Plutus script validating auction transactions"
     , validatorParameters =
         [ MkParameterBlueprint
             { parameterTitle = Just "Parameters"
@@ -77,7 +96,7 @@ myValidator =
     , validatorDatum = Nothing
     , validatorCompiled = do
         let script = auctionValidatorScript auctionParams
-        let code = Short.fromShort (serialiseCompiledCode script) 
+        let code = Short.fromShort (serialiseCompiledCode script)
         Just (compiledValidator PlutusV2 code)
     }
 
